5269f1032409d6feb36cd6601d75111ea9f4dd5c
agarwalayush9 <ayushag.cse@gmail.com>
2024-07-03 17:12:20 +0530

    initial commit

---
+++ Shelves-Admin/Shelves_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shelves_AdminApp.swift
+//  Shelves-Admin
+//
+//  Created by Ayush Agarwal on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Shelves_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
