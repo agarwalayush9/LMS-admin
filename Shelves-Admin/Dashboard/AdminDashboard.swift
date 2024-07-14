3853538450e6a3b11e57011a3f3cd2e5aa434d3a
Mohit <92150824+MKG26@users.noreply.github.com>
2024-07-14 20:01:03 +0530

    .

--- Shelves-Admin/Dashboard/AdminDashboard.swift
+++ Shelves-Admin/Dashboard/AdminDashboard.swift
@@ -22,7 +22,7 @@ struct AdminDashboard: View {
             VStack{
                 HStack(spacing : 0){
                     VStack(alignment: .leading, spacing: 16){
-                        userName(userName: "User")
+                        userName(userName: "Admin")
                         todayDateAndTime()
                         
                     }
